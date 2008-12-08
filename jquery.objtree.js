ce1731a3c7c9db8eb529737a4cfb4baba9ab4c48
thatcher <thatcher.christopher@gmail.com>
2008-12-08 12:14:23 -0500

    removed the http stuff since jquery has that.  default attr prefix changed to $.  added ns colon replcement, defaults to $.  added static methods to jquery namespace.

--- jquery.objtree.js
+++ jquery.objtree.js
@@ -1,28 +1,50 @@
-// ========================================================================
-//  XML.ObjTree -- XML source code from/to JavaScript object like E4X
-// ========================================================================
 
-if ( typeof(XML) == 'undefined' ) XML = function() {};
+(function($){
+
+$.objtree = function(settings){
+  $.extend(ObjTree.prototype, settings);  
+};
+
+$.xml2js = function(xml, opts){
+    var objtree = $.extend(new ObjTree(), opts||{});
+    return objtree.parseXML(xml);
+};
+
+$.dom2js = function(dom, opts){
+    var objtree = $.extend(new ObjTree(), opts||{});
+    return objtree.parseDOM(dom);
+};
+
+$.js2xml = function(js, opts){
+    var objtree = $.extend(new ObjTree(), opts||{});
+    return objtree.writeXML(js);
+};
+
+// ========================================================================
+//  ObjTree -- XML source code from/to JavaScript object like E4X
+// ========================================================================
+// A thin jQuery wrapper for Yusuke Kawasaki's ObjTree.
 
 //  constructor
 
-XML.ObjTree = function () {
+var  ObjTree = function () {
     return this;
 };
 
 //  class variables
 
-XML.ObjTree.VERSION = "0.24";
+ObjTree.VERSION = "0.24";
 
 //  object prototype
 
-XML.ObjTree.prototype.xmlDecl = '<?xml version="1.0" encoding="UTF-8" ?>\n';
-XML.ObjTree.prototype.attr_prefix = '-';
-XML.ObjTree.prototype.overrideMimeType = 'text/xml';
+ObjTree.prototype.xmlDecl = '<?xml version="1.0" encoding="UTF-8" ?>\n';
+ObjTree.prototype.attr_prefix = '$';
+ObjTree.prototype.ns_colon = '$';
+ObjTree.prototype.overrideMimeType = 'text/xml';
+
 
 //  method: parseXML( xmlsource )
-
-XML.ObjTree.prototype.parseXML = function ( xml ) {
+ObjTree.prototype.parseXML = function ( xml ) {
     var root;
     if ( window.DOMParser ) {
         var xmldom = new DOMParser();
@@ -40,63 +62,9 @@ XML.ObjTree.prototype.parseXML = function ( xml ) {
     return this.parseDOM( root );
 };
 
-//  method: parseHTTP( url, options, callback )
-
-XML.ObjTree.prototype.parseHTTP = function ( url, options, callback ) {
-    var myopt = {};
-    for( var key in options ) {
-        myopt[key] = options[key];                  // copy object
-    }
-    if ( ! myopt.method ) {
-        if ( typeof(myopt.postBody) == "undefined" &&
-             typeof(myopt.postbody) == "undefined" &&
-             typeof(myopt.parameters) == "undefined" ) {
-            myopt.method = "get";
-        } else {
-            myopt.method = "post";
-        }
-    }
-    if ( callback ) {
-        myopt.asynchronous = true;                  // async-mode
-        var __this = this;
-        var __func = callback;
-        var __save = myopt.onComplete;
-        myopt.onComplete = function ( trans ) {
-            var tree;
-            if ( trans && trans.responseXML && trans.responseXML.documentElement ) {
-                tree = __this.parseDOM( trans.responseXML.documentElement );
-            } else if ( trans && trans.responseText ) {
-                tree = __this.parseXML( trans.responseText );
-            }
-            __func( tree, trans );
-            if ( __save ) __save( trans );
-        };
-    } else {
-        myopt.asynchronous = false;                 // sync-mode
-    }
-    var trans;
-    if ( typeof(HTTP) != "undefined" && HTTP.Request ) {
-        myopt.uri = url;
-        var req = new HTTP.Request( myopt );        // JSAN
-        if ( req ) trans = req.transport;
-    } else if ( typeof(Ajax) != "undefined" && Ajax.Request ) {
-        var req = new Ajax.Request( url, myopt );   // ptorotype.js
-        if ( req ) trans = req.transport;
-    }
-//  if ( trans && typeof(trans.overrideMimeType) != "undefined" ) {
-//      trans.overrideMimeType( this.overrideMimeType );
-//  }
-    if ( callback ) return trans;
-    if ( trans && trans.responseXML && trans.responseXML.documentElement ) {
-        return this.parseDOM( trans.responseXML.documentElement );
-    } else if ( trans && trans.responseText ) {
-        return this.parseXML( trans.responseText );
-    }
-}
-
 //  method: parseDOM( documentroot )
 
-XML.ObjTree.prototype.parseDOM = function ( root ) {
+ObjTree.prototype.parseDOM = function ( root ) {
     if ( ! root ) return;
 
     this.__force_array = {};
@@ -120,7 +88,7 @@ XML.ObjTree.prototype.parseDOM = function ( root ) {
 
 //  method: parseElement( element )
 
-XML.ObjTree.prototype.parseElement = function ( elem ) {
+ObjTree.prototype.parseElement = function ( elem ) {
     //  COMMENT_NODE
     if ( elem.nodeType == 7 ) {
         return;
@@ -183,31 +151,41 @@ XML.ObjTree.prototype.parseElement = function ( elem ) {
 
 //  method: addNode( hash, key, count, value )
 
-XML.ObjTree.prototype.addNode = function ( hash, key, cnts, val ) {
+ObjTree.prototype.addNode = function ( hash, key, cnts, val ) {
+    var jskey = this.removeColon(key);
     if ( this.__force_array[key] ) {
-        if ( cnts == 1 ) hash[key] = [];
-        hash[key][hash[key].length] = val;      // push
+        if ( cnts == 1 ) hash[jskey] = [];
+        hash[jskey][hash[jskey].length] = val;      // push
     } else if ( cnts == 1 ) {                   // 1st sibling
-        hash[key] = val;
+        hash[jskey] = val;
     } else if ( cnts == 2 ) {                   // 2nd sibling
-        hash[key] = [ hash[key], val ];
+        hash[jskey] = [ hash[jskey], val ];
     } else {                                    // 3rd sibling and more
-        hash[key][hash[key].length] = val;
+        hash[jskey][hash[jskey].length] = val;
     }
 };
 
 //  method: writeXML( tree )
 
-XML.ObjTree.prototype.writeXML = function ( tree ) {
+ObjTree.prototype.writeXML = function ( tree ) {
     var xml = this.hash_to_xml( null, tree );
     return this.xmlDecl + xml;
 };
 
-//  method: hash_to_xml( tagName, tree )
+//  method: replaceColon( tagName, tree )
+ObjTree.prototype.replaceColon = function(name){
+    return name ? (name.substring(0,1)+name.substring(1).replace(this.ns_colon,':')): name;
+};
+//  method: replaceColon( tagName, tree )
+ObjTree.prototype.removeColon = function(name){
+    return name ? (name.replace(':',this.ns_colon)): name;
+};
 
-XML.ObjTree.prototype.hash_to_xml = function ( name, tree ) {
+//  method: hash_to_xml( tagName, tree )
+ObjTree.prototype.hash_to_xml = function ( name, tree ) {
     var elem = [];
     var attr = [];
+    name = this.replaceColon(name);
     for( var key in tree ) {
         if ( ! tree.hasOwnProperty(key) ) continue;
         var val = tree[key];
@@ -222,7 +200,7 @@ XML.ObjTree.prototype.hash_to_xml = function ( name, tree ) {
                 elem[elem.length] = this.scalar_to_xml( key, val );
             }
         } else {
-            attr[attr.length] = " "+(key.substring(1))+'="'+(this.xml_escape( val ))+'"';
+            attr[attr.length] = " "+(this.replaceColon(key).substring(1))+'="'+(this.xml_escape( val ))+'"';
         }
     }
     var jattr = attr.join("");
@@ -243,8 +221,9 @@ XML.ObjTree.prototype.hash_to_xml = function ( name, tree ) {
 
 //  method: array_to_xml( tagName, array )
 
-XML.ObjTree.prototype.array_to_xml = function ( name, array ) {
+ObjTree.prototype.array_to_xml = function ( name, array ) {
     var out = [];
+    name = this.replaceColon(name);
     for( var i=0; i<array.length; i++ ) {
         var val = array[i];
         if ( typeof(val) == "undefined" || val == null ) {
@@ -262,7 +241,8 @@ XML.ObjTree.prototype.array_to_xml = function ( name, array ) {
 
 //  method: scalar_to_xml( tagName, text )
 
-XML.ObjTree.prototype.scalar_to_xml = function ( name, text ) {
+ObjTree.prototype.scalar_to_xml = function ( name, text ) {
+    name = this.replaceColon(name);
     if ( name == "#text" ) {
         return this.xml_escape(text);
     } else {
@@ -272,259 +252,9 @@ XML.ObjTree.prototype.scalar_to_xml = function ( name, text ) {
 
 //  method: xml_escape( text )
 
-XML.ObjTree.prototype.xml_escape = function ( text ) {
+ObjTree.prototype.xml_escape = function ( text ) {
     return String(text).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;');
 };
 
-/*
-// ========================================================================
 
-=head1 NAME
-
-XML.ObjTree -- XML source code from/to JavaScript object like E4X
-
-=head1 SYNOPSIS
-
-    var xotree = new XML.ObjTree();
-    var tree1 = {
-        root: {
-            node: "Hello, World!"
-        }
-    };
-    var xml1 = xotree.writeXML( tree1 );        // object tree to XML source
-    alert( "xml1: "+xml1 );
-
-    var xml2 = '<?xml version="1.0"?><response><error>0</error></response>';
-    var tree2 = xotree.parseXML( xml2 );        // XML source to object tree
-    alert( "error: "+tree2.response.error );
-
-=head1 DESCRIPTION
-
-XML.ObjTree class is a parser/generater between XML source code
-and JavaScript object like E4X, ECMAScript for XML.
-This is a JavaScript version of the XML::TreePP module for Perl.
-This also works as a wrapper for XMLHTTPRequest and successor to JKL.ParseXML class
-when this is used with prototype.js or JSAN's HTTP.Request class.
-
-=head2 JavaScript object tree format
-
-A sample XML source:
-
-    <?xml version="1.0" encoding="UTF-8"?>
-    <family name="Kawasaki">
-        <father>Yasuhisa</father>
-        <mother>Chizuko</mother>
-        <children>
-            <girl>Shiori</girl>
-            <boy>Yusuke</boy>
-            <boy>Kairi</boy>
-        </children>
-    </family>
-
-Its JavaScript object tree like JSON/E4X:
-
-    {
-        'family': {
-            '-name':    'Kawasaki',
-            'father':   'Yasuhisa',
-            'mother':   'Chizuko',
-            'children': {
-                'girl': 'Shiori'
-                'boy': [
-                    'Yusuke',
-                    'Kairi'
-                ]
-            }
-        }
-    };
-
-Each elements are parsed into objects:
-
-    tree.family.father;             # the father's given name.
-
-Prefix '-' is inserted before every attributes' name.
-
-    tree.family["-name"];           # this family's family name
-
-A array is used because this family has two boys.
-
-    tree.family.children.boy[0];    # first boy's name
-    tree.family.children.boy[1];    # second boy's name
-    tree.family.children.girl;      # (girl has no other sisiters)
-
-=head1 METHODS
-
-=head2 xotree = new XML.ObjTree()
-
-This constructor method returns a new XML.ObjTree object.
-
-=head2 xotree.force_array = [ "rdf:li", "item", "-xmlns" ];
-
-This property allows you to specify a list of element names
-which should always be forced into an array representation.
-The default value is null, it means that context of the elements
-will determine to make array or to keep it scalar.
-
-=head2 xotree.attr_prefix = '@';
-
-This property allows you to specify a prefix character which is
-inserted before each attribute names.
-Instead of default prefix '-', E4X-style prefix '@' is also available.
-The default character is '-'.
-Or set '@' to access attribute values like E4X, ECMAScript for XML.
-The length of attr_prefix must be just one character and not be empty.
-
-=head2 xotree.xmlDecl = '';
-
-This library generates an XML declaration on writing an XML code per default.
-This property forces to change or leave it empty.
-
-=head2 tree = xotree.parseXML( xmlsrc );
-
-This method loads an XML document using the supplied string
-and returns its JavaScript object converted.
-
-=head2 tree = xotree.parseDOM( domnode );
-
-This method parses a DOM tree (ex. responseXML.documentElement)
-and returns its JavaScript object converted.
-
-=head2 tree = xotree.parseHTTP( url, options );
-
-This method loads a XML file from remote web server
-and returns its JavaScript object converted.
-XMLHTTPRequest's synchronous mode is always used.
-This mode blocks the process until the response is completed.
-
-First argument is a XML file's URL
-which must exist in the same domain as parent HTML file's.
-Cross-domain loading is not available for security reasons.
-
-Second argument is options' object which can contains some parameters:
-method, postBody, parameters, onLoading, etc.
-
-This method requires JSAN's L<HTTP.Request> class or prototype.js's Ajax.Request class.
-
-=head2 xotree.parseHTTP( url, options, callback );
-
-If a callback function is set as third argument,
-XMLHTTPRequest's asynchronous mode is used.
-
-This mode calls a callback function with XML file's JavaScript object converted
-after the response is completed.
-
-=head2 xmlsrc = xotree.writeXML( tree );
-
-This method parses a JavaScript object tree
-and returns its XML source generated.
-
-=head1 EXAMPLES
-
-=head2 Text node and attributes
-
-If a element has both of a text node and attributes
-or both of a text node and other child nodes,
-text node's value is moved to a special node named "#text".
-
-    var xotree = new XML.ObjTree();
-    var xmlsrc = '<span class="author">Kawasaki Yusuke</span>';
-    var tree = xotree.parseXML( xmlsrc );
-    var class = tree.span["-class"];        # attribute
-    var name  = tree.span["#text"];         # text node
-
-=head2 parseHTTP() method with HTTP-GET and sync-mode
-
-HTTP/Request.js or prototype.js must be loaded before calling this method.
-
-    var xotree = new XML.ObjTree();
-    var url = "http://example.com/index.html";
-    var tree = xotree.parseHTTP( url );
-    xotree.attr_prefix = '@';                   // E4X-style
-    alert( tree.html["@lang"] );
-
-This code shows C<lang=""> attribute from a X-HTML source code.
-
-=head2 parseHTTP() method with HTTP-POST and async-mode
-
-Third argument is a callback function which is called on onComplete.
-
-    var xotree = new XML.ObjTree();
-    var url = "http://example.com/mt-tb.cgi";
-    var opts = {
-        postBody:   "title=...&excerpt=...&url=...&blog_name=..."
-    };
-    var func = function ( tree ) {
-        alert( tree.response.error );
-    };
-    xotree.parseHTTP( url, opts, func );
-
-This code send a trackback ping and shows its response code.
-
-=head2 Simple RSS reader
-
-This is a RSS reader which loads RDF file and displays all items.
-
-    var xotree = new XML.ObjTree();
-    xotree.force_array = [ "rdf:li", "item" ];
-    var url = "http://example.com/news-rdf.xml";
-    var func = function( tree ) {
-        var elem = document.getElementById("rss_here");
-        for( var i=0; i<tree["rdf:RDF"].item.length; i++ ) {
-            var divtag = document.createElement( "div" );
-            var atag = document.createElement( "a" );
-            atag.href = tree["rdf:RDF"].item[i].link;
-            var title = tree["rdf:RDF"].item[i].title;
-            var tnode = document.createTextNode( title );
-            atag.appendChild( tnode );
-            divtag.appendChild( atag );
-            elem.appendChild( divtag );
-        }
-    };
-    xotree.parseHTTP( url, {}, func );
-
-=head2  XML-RPC using writeXML, prototype.js and parseDOM
-
-If you wish to use prototype.js's Ajax.Request class by yourself:
-
-    var xotree = new XML.ObjTree();
-    var reqtree = {
-        methodCall: {
-            methodName: "weblogUpdates.ping",
-            params: {
-                param: [
-                    { value: "Kawa.net xp top page" },  // 1st param
-                    { value: "http://www.kawa.net/" }   // 2nd param
-                ]
-            }
-        }
-    };
-    var reqxml = xotree.writeXML( reqtree );       // JS-Object to XML code
-    var url = "http://example.com/xmlrpc";
-    var func = function( req ) {
-        var resdom = req.responseXML.documentElement;
-        xotree.force_array = [ "member" ];
-        var restree = xotree.parseDOM( resdom );   // XML-DOM to JS-Object
-        alert( restree.methodResponse.params.param.value.struct.member[0].value.string );
-    };
-    var opt = {
-        method:         "post",
-        postBody:       reqxml,
-        asynchronous:   true,
-        onComplete:     func
-    };
-    new Ajax.Request( url, opt );
-
-=head1 AUTHOR
-
-Yusuke Kawasaki http://www.kawa.net/
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (c) 2005-2006 Yusuke Kawasaki. All rights reserved.
-This program is free software; you can redistribute it and/or
-modify it under the Artistic license. Or whatever license I choose,
-which I will do instead of keeping this documentation like it is.
-
-=cut
-// ========================================================================
-*/
+})(jQuery);
