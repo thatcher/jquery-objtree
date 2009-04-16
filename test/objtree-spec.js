0f091807b5b24d55114345da6c01adec0edbbe53
thatcher <thatcher@christopher-thatchers-macbook-air.local>
2009-04-16 10:51:46 -0400

    updated project to act as jquery-jspath plugin

---
+++ test/objtree-spec.js
@@ -0,0 +1,17 @@
+(function(_){
+  	// RSpec/Bacon Style
+	with (jqUnit) {
+		
+		//tests basic init functionality
+		describe('jQuery jsPath ObjTree', '_.xml2js', {
+			before: function(){
+			}
+		}).pending('should do something awesome', function(){
+			// It doesnt matter what you put here it wont be run until
+			// you change this to an actual spec
+			ok(false);
+		});
+			
+	}
+    
+})(jsPath);
